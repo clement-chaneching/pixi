f70ac4081ec1f446a3bb2d04caae829c8763fcee
Clement Chane <clement.chane-ching@enea-consulting.com>
2024-04-21 15:18:43 +1000

    adding gcs auth as feature

--- src/utils/reqwest.rs
+++ src/utils/reqwest.rs
@@ -7,6 +7,9 @@ use rattler_networking::{
     AuthenticationMiddleware, AuthenticationStorage, MirrorMiddleware, OciMiddleware,
 };
 
+#[cfg(feature = "google-cloud-auth")]
+use rattler_networking::GCSMiddleware;
+
 use reqwest::Client;
 use reqwest_middleware::{ClientBuilder, ClientWithMiddleware};
 use std::collections::HashMap;
@@ -103,6 +106,11 @@ pub(crate) fn build_reqwest_clients(config: Option<&Config>) -> (Client, ClientW
         client_builder = client_builder
             .with(mirror_middleware(&config))
             .with(oci_middleware());
+    }
+
+    #[cfg(feature = "google-cloud-auth")]
+    {
+        client_builder = client_builder.with(GCSMiddleware);
     }
 
     client_builder = client_builder.with_arc(Arc::new(
